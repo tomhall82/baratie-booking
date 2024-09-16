fda4c75c16477ee68ac4a2d289fb404c6a7fdc9b
tomhall82 <tom.hall82@yahoo.co.uk>
2024-09-16 14:50:55 +0100

    add a booking model

--- baratie/settings.py
+++ baratie/settings.py
@@ -95,6 +95,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CRSF_TRUSTED_ORIGINS = ['https://baratie.herokuapp.com']
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- book/models.py
+++ book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    booking_party_size = models.IntegerField()
+    booking_message = models.TextField()
