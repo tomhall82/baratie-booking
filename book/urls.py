557b473f66a7870e72f8d2d91b4abdb4e1ea7930
tomhall82 <tom.hall82@yahoo.co.uk>
2024-10-03 18:07:58 +0100

    added booking_list to enable users to view bookings

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookingCreateView
+from .views import BookingCreateView, BookingListView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('book/', BookingCreateView.as_view(), name='book'),
     path('booking-success/', TemplateView.as_view(template_name="booking_success.html"), name='booking_success'),
+    path('my-bookings/', BookingListView.as_view(), name='booking_list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Booking
 from .forms import BookingForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -22,3 +22,13 @@ class BookingCreateView(LoginRequiredMixin, CreateView):
         # If the form is invalid, show an error message
         messages.error(self.request, 'Please correct the errors below.')
         return super().form_invalid(form)
+    
+# List view to show all bookings for the logged-in user
+class BookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = 'booking_list.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        # Filter bookings by the logged-in user
+        return Booking.objects.filter(user=self.request.user).order_by('booking_date')
