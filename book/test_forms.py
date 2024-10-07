c5826d627c20abe1b681515274be598a58e6faba
tomhall82 <tom.hall82@yahoo.co.uk>
2024-10-07 20:45:09 +0100

    additional tests added to test_forms.py

--- book/test_forms.py
+++ book/test_forms.py
@@ -13,3 +13,15 @@ class BookingFormTest(TestCase):
         }
         form = BookingForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    def test_form_missing_required_fields(self):
+        form_data = {
+            'booking_name': '',  # Empty name
+            'booking_date': date.today(),
+            'booking_time': time(), # No time specified
+            'party_size': 2,
+        }
+        form = BookingForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('booking_name', form.errors)
+        self.assertIn('booking_time', form.errors)
